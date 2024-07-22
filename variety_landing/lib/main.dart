28445aa75bc1cb8c4c9844245cb93b77351fa5e5
montrell <computertechnicals@yahoo.com>
2024-07-22 14:23:43 -0400

    added analytics to index.html

--- variety_landing/lib/main.dart
+++ variety_landing/lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:variety_landing/firebase_options.dart';
 import 'package:variety_landing/view/home.dart';
 import 'package:variety_landing/view/policy.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
