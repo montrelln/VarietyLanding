992c2564dad7d05ddefadf171da4a845675f2456
montrell <computertechnicals@yahoo.com>
2024-07-22 13:50:36 -0400

    ensured firebase was inititalized

--- variety_landing/lib/main.dart
+++ variety_landing/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:variety_landing/firebase_options.dart';
 import 'package:variety_landing/view/home.dart';
 import 'package:variety_landing/view/policy.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
